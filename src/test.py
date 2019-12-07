a93a1bdc67e3259b445d6cccd77d894b13910df6
Mikkel Hooge Sørensen <Mikkel Hooge Srensen>
2019-12-07 13:06:28 +0100

    Fixed error in method detecting whether needy module is active.

--- src/test.py
+++ src/test.py
@@ -1,14 +1,6 @@
 import cv2
-import features.whos_first
-import features.needy_knob
-import features.maze
+import features.needy_util
 
-img = cv2.imread("../resources/misc/error_imgs/4.png", cv2.IMREAD_COLOR)
-details = features.whos_first.get_characters(img)
-masks = details[0]
-for mask in masks:
-    cv2.imshow("Test", mask)
-    key = cv2.waitKey(0)
-    if key == ord('q'):
-        exit(0)
-    cv2.destroyWindow("Test")
+img = cv2.imread("../resources/misc/error_imgs/16.png", cv2.IMREAD_COLOR)
+#img = cv2.imread("../resources/training_images/needy_discharge/000.png", cv2.IMREAD_COLOR)
+print(features.needy_util.is_active(img))
